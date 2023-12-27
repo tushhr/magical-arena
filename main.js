9e2fc1376bb538202b5ea465b56605ed6cc34e67
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-12-28 01:21:03 +0530

    feat: scale the arena for multiple users

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
+
 function getInput(question) {
     return new Promise((resolve) => {
         rl.question(question, (answer) => {
@@ -16,30 +20,38 @@ function getInput(question) {
     });
 }
 
-function sleep(seconds) {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-}
-
 const getFloatingInput = async (i, name) => {
     do {
         const input = await getInput(`Enter the ${name} of Player${i + 1}: `);
 
-        if (!isNaN(parseFloat(input))) {
+        if (!isNaN(parseFloat(input)) && parseFloat(input) > 0) {
             return parseFloat(input);
         }
         console.log("Invalid Input. Please enter a number");
     } while (true);
 }
 
-const startFight = async (magicalArena) => { 
-    while(true) {
+const fight = async (players) => { 
+    for(let j = 0; j < players.length - 1; j++) {
         const startFight = await getInput(`Start Fight? (y/n): `);
 
         if (startFight === 'y') {
-            magicalArena.fight();
-            break;
+            playersAvailableForFight = ""
+            for(let i = 0; i < players.length; i++) {
+                if(players[i].health > 0) {
+                    playersAvailableForFight += ` ${i}`
+                }
+            }
+
+            console.log("Players who are still alive: " + playersAvailableForFight);
+            const playerId1 = await getInput(`Enter ID of Player1: `);
+            const playerId2 = await getInput(`Enter ID of Player2: `);
+
+            const magicalArena = new MagicalArena(players[playerId1], players[playerId2]);
+            magicalArena.fight()
+
         } else if (startFight === 'n') {
-            const waitingTime = await getInput(`How long should we wait? (in seconds) `);
+            const waitingTime = await getFloatingInput(`How long should we wait? (in seconds) `);
             await sleep(waitingTime);
         } else {
             console.log("Invalid Input. Please check valid options");
@@ -49,7 +61,9 @@ const startFight = async (magicalArena) => {
 
 async function main() {
     const players = [];
-    for(let i = 0; i < 2; i++) {
+    const numberOfPlayers = await getInput("Enter Number of Players: ");
+
+    for(let i = 0; i < numberOfPlayers; i++) {
         const name = await getInput(`Enter Name of Player${i + 1}: `);
 
         const health = await getFloatingInput(i, "Health")
@@ -59,8 +73,7 @@ async function main() {
         players.push(new Player(name, health, strength, defense));
     }
 
-    const magicalArena = new MagicalArena(players[0], players[1]);
-    await startFight(magicalArena);
+    await fight(players);
 
     rl.close();
 }
