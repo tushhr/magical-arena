44ab46ba38944bfa3a41f4c29ff2c71b94a242bd
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-12-27 20:13:56 +0530

    feat: add attack method for MagicalArena class

--- controllers/magicalArena.js
+++ controllers/magicalArena.js
@@ -3,4 +3,13 @@ class MagicalArena {
         this.player1 = player1;
         this.player2 = player2;
     }
+    
+    attack(attacker, defender) {
+        const damage = attacker.rollDice() * attacker.strength - defender.defense;
+
+        if (damage > 0) {
+            defender.health -= damage;
+        }
+        return damage;
+    }
 }
