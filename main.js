9c89162240dea706ad9dd254d0a1e18df737a42c
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-12-27 22:46:14 +0530

    feat: add main function for reading inputs

---
+++ main.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askQuestion(question) {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
+    });
+}
+
+const getFloatingInput = async (i, name) => {
+    do {
+        const input = await askQuestion(`Enter the ${name} of Player${i + 1}: `);
+
+        if (!isNaN(parseFloat(input))) {
+            return parseFloat(input);
+        }
+    } while (true);
+}
+
+async function main() {
+    for(let i = 0; i < 2; i++) {
+        const name = await askQuestion(`Enter Name of Player${i + 1}: `);
+
+        const health = await getFloatingInput(i, "Health")
+        const strength = await getFloatingInput(i, "Strength")
+        const defense = await getFloatingInput(i, "Defense")
+    }
+
+    rl.close();
+}
+
+main();
