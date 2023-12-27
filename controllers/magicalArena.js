f1296be7551b978a2d2d53d0e3eefec0739aee66
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-12-27 20:18:59 +0530

    feat: add method for attack & fight for MagicalArena class

--- controllers/magicalArena.js
+++ controllers/magicalArena.js
@@ -5,11 +5,23 @@ class MagicalArena {
     }
     
     attack(attacker, defender) {
+        console.log("Attacker: " + attacker.name + " Defender: " + defender.name);
         const damage = attacker.rollDice() * attacker.strength - defender.defense;
 
+        console.log("Damage: " + damage);
         if (damage > 0) {
             defender.health -= damage;
         }
+
+        console.log("Defender Health: " + defender.health);
         return damage;
+    }
+
+    fight() {
+        // assuming player1 has lower health than player2
+        while (this.player1.isAlive() && this.player2.isAlive()) {
+            this.attack(this.player1, this.player2);
+            this.attack(this.player2, this.player1);
+        }
     }
 }
